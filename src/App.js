b4d74d544f626991d199310260cbf16fa80a3034
Balu Ravi <balu89.rs@gmail.com>
2023-07-23 19:08:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [generateExcuse, setGenerateExcuse] = useState("");
+  const fetchExcuseData = (excuse) => {
+
+    axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`)
+      //.then((response) => response.data.map((item) => setExcuse(item.excuse)))
+      .then((response) => setGenerateExcuse(response.data[0].excuse));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1 style={{ marginBottom: "0px" }}>Generate An Excuse</h1>
+      <button onClick={() => fetchExcuseData("party")} >Party</button><br></br>
+      <button onClick={() => fetchExcuseData("family")} className="margin-top">Family</button><br></br>
+      <button onClick={() => fetchExcuseData("office")} className="margin-top">Office</button><br></br>
+
+      <h1>{generateExcuse}</h1>
+
     </div>
   );
 }
