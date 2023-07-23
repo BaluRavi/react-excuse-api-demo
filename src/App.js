11b728e1db5bbc9a3f6554674dde777dfaffe1e4
Balu Ravi <balu89.rs@gmail.com>
2023-07-23 19:46:59 +0530

    Added Error Handling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,24 @@ function App() {
   const [generateExcuse, setGenerateExcuse] = useState("");
   const fetchExcuseData = (excuse) => {
 
-    axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`)
+    axios.get(`https://excuser-three.vercel.app/v12/excuse/${excuse}/`)
       //.then((response) => response.data.map((item) => setExcuse(item.excuse)))
-      .then((response) => setGenerateExcuse(response.data[0].excuse));
+      .then((response) => setGenerateExcuse(response.data[0].excuse))
+      .catch((error) => {
+        if (error.response) {
+          console.log(error.response);
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        }
+        else if (error.request) {
+          console.log(error.request)
+
+        }
+        else {
+          console.log('Error is ' + error.message);
+        }
+      })
   }
 
   return (
